de64dccee750d2f866b56d6b0902f126643a665c
Nina <ninkr@list.ru>
2020-09-15 15:13:13 +0300

    Доработка полей класса

--- Student.cs
+++ Student.cs
@@ -6,34 +6,49 @@ using System.Threading.Tasks;
 
 namespace Test
 {
-    class Student
+        
+    public struct Contacts
     {
-        public struct Contacts
+        public string Tel { set; get; }
+        public string address;
+    }
+
+    public class Student
+    {
+
+        public readonly string firstName;
+        public readonly string lastName;
+        public readonly DateTime birthday;
+        public byte Age { get; private set; }
+        public Contacts contacts;
+        public List<byte> balls;
+        public double Average { get; private set; }
+
+        public Student(string firstName, string lastName, DateTime birthday)
         {
-            public string Tel { set; get; }
-            public string address;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthday = birthday;
         }
 
-        public class Student
-        {
-            public string firstName;
-            public string lastName;
-            public byte age;
-            public Contacts contacts;
-            public int[] balls = new int[5];
-            public double Average { get; private set; }
-            public double AverageCalc()
+        public double AverageCalc()
             {
                 int sum = 0;
                 for(int i = 0; i < 5; i++)
                 {
                     sum += balls[i];
                 }
+                //balls.Average();
 
                 Average = sum / 5;
 
                 return Average;
             }
+            public byte AgeCalc()
+            {
+
+                return Age;
+            }
         }
-    }
+    
 }
